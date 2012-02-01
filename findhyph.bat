c9f61735896267f527be98341c5ca1e847bd130b
mbudaj <m.budaj@gmail.com>
2012-02-01 12:00:00 +0100

    release 3.0

--- findhyph.bat
+++ findhyph.bat
@@ -1,4 +1,17 @@
+@rem = '--*-Perl-*--
+@echo off
+if "%OS%" == "Windows_NT" goto WinNT
+perl -x -S "%0" %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto endofperl
+:WinNT
+perl -x -S %0 %*
+if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
+if %errorlevel% == 9009 echo You do not have Perl in your PATH.
+if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
+goto endofperl
+@rem ';
 #!/usr/bin/perl
+#line 15
 
 use warnings;
 use Getopt::Std;
@@ -360,3 +373,6 @@ any later version.
 Copyright (c) Martin Budaj E<lt>m.b@speleo.skE<gt> 2000, 2001, 2009, 2012
 
 =cut
+
+__END__
+:endofperl
